51290a35fb6d59ae3ed71d5f499ae874c5c8e327
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-09-07 18:00:00 +0200

    Update 001-estricura.py

--- Acceso a datos/001- Manejo de ficheros/001- Clases asociadas a las operaciones de gestion/001-estricura.py
+++ Acceso a datos/001- Manejo de ficheros/001- Clases asociadas a las operaciones de gestion/001-estricura.py
@@ -1,3 +1,3 @@
 archivo = open("agenda.txt",'w') # Flag indica el modo de apertura
-archivo.write("Hola soy Jose Vicente")
+archivo.write("Hola soy Andrei Buga")
 archivo.close() # cerramos siempre los recursos que hayamos abierto
