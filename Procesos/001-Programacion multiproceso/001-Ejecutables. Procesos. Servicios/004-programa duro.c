e13e2383e5a7f78c4676802825c2d8adaf5274b5
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-09-07 20:34:40 +0200

    Update 004-programa duro.c

--- Procesos/001-Programacion multiproceso/001-Ejecutables. Procesos. Servicios/004-programa duro.c
+++ Procesos/001-Programacion multiproceso/001-Ejecutables. Procesos. Servicios/004-programa duro.c
@@ -2,7 +2,7 @@
 
 int main() {
     float numero = 1.00000000054;
-    for(int i = 0;i<100000000;i++){
+    for(int i = 0;i<10000000000000000;i++){
     	numero = numero*1.00000000534;
     }
     return 0;
