936e452d5e219e9d49ea9259b39dae1dfd1a1a18
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-09-10 12:28:47 +0200

    Create 011-nueva tabla.py

---
+++ Acceso a datos/001- Manejo de ficheros/002-Formas de acceso a un archivo/011-nueva tabla.py
@@ -0,0 +1,8 @@
+import os
+ruta = "C:\\Users\\Usuario\\Desktop\\Acceso a datos\\001- Manejo de ficheros\\002-Formas de acceso a un archivo\\"
+base = "clientes"
+
+archivo = open(ruta+base+"/personas.csv",'w')
+archivo.write("1,Andrei,Buga")
+
+archivo.close()
