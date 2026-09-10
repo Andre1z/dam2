cce0609fa8f84beedbcd87df44684db9462f3dca
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-09-10 12:27:59 +0200

    Create 010-creamos una base de datos.py

---
+++ Acceso a datos/001- Manejo de ficheros/002-Formas de acceso a un archivo/010-creamos una base de datos.py
@@ -0,0 +1,4 @@
+import os
+ruta = "C:\\Users\\Usuario\\Desktop\\Acceso a datos\\001- Manejo de ficheros\\002-Formas de acceso a un archivo\\"
+base = input("Introduce tu base de datos:")
+os.mkdir(ruta+base)
